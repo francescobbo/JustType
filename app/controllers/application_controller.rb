bd45e196240e8a24e2f36be41449f6638db5f0eb
Francesco Boffa <fra.boffa@gmail.com>
2017-07-25 16:07:31 +0100

    More meta tags

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,12 +8,19 @@ class ApplicationController < ActionController::Base
   def default_meta_tags
     {
       site: Settings.site_name,
+      reverse: true,
       viewport: 'width=device-width,minimum-scale=1,initial-scale=1',
       og: {
+        type: 'website',
+        url: request.original_url,
+        title: "#{meta_tags.meta_tags[:title]} | #{Settings.site_name}",
+        description: meta_tags.meta_tags[:description],
         locale: Settings.locale,
         site_name: Settings.site_name
       },
       twitter: {
+        title: "#{meta_tags.meta_tags[:title]} | #{Settings.site_name}",
+        description: meta_tags.meta_tags[:description],
         card: 'summary'
       }
     }
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,20 @@ class PostsController < ApplicationController
   def show
     post = Post.visible.friendly.find(params[:slug])
 
+    set_meta_tags({
+      title: post.title,
+      og: {
+        type: 'article',
+        updated_time: post.updated_at.iso8601
+      },
+      article: {
+        section: "Uncategorized",
+        published_time: post.published_at.iso8601,
+        modified_time: post.updated_at.iso8601
+      },
+      "DC.date.issued" => post.published_at.iso8601
+    })
+
     render locals: {
       post: post
     }
