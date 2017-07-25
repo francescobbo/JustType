9b999a9958b955652760176c29c881e9f2ac8183
Francesco Boffa <fra.boffa@gmail.com>
2017-07-25 16:40:02 +0100

    Embedding CSS in head if asked

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def read_css_asset(name)
+    if Rails.application.assets && Rails.application.assets[name]
+      content = Rails.application.assets[name].to_s
+    else
+      content = File.read "#{Rails.root}/public#{stylesheet_path(name, host: nil)}"
+    end
+
+    content.gsub(/\n\s*/, '')
+  end
 end
