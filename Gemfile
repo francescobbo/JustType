485279713bc62fb0cd328ff2ba2e3f6117039bd8
Francesco Boffa <fra.boffa@gmail.com>
2017-07-25 17:27:53 +0100

    Added ServiceWorker for Progressive Web Applications

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'config'
 gem 'bcrypt'
 gem 'friendly_id'
 gem 'meta-tags'
+gem 'serviceworker-rails'
 
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
---
+++ app/assets/javascripts/web.js
@@ -0,0 +1,7 @@
+if (navigator.serviceWorker) {
+  navigator.serviceWorker.register('/serviceworker.js', { scope: './' })
+    .then(function(reg) {
+      console.log('[Page] Service worker registered!');
+    });
+}
+
